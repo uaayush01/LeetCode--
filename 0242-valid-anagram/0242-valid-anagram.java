8d84aa8e2fe71f324b6f2526e7415eb9af1a1b61
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-06-24 22:15:12 -0400

    Time: 4 ms (70.81%), Space: 44.2 MB (57.08%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // if length of both string is not equal 
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char[] str1=s.toCharArray();
+        char [] str2=t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        // alternate way
+        for(int i=0;i<s.length();i++){
+            if(str1[i]!=str2[i]){
+                return false;
+            }
+        }
+        return true;
+        // return Arrays.equals(str1,str2);
+    }
+}
