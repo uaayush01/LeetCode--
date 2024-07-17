dcd20a3b9a0691befff58f21e6cf71beb3082bd9
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-07-17 11:01:12 -0400

    Time: 1 ms (96.16%), Space: 45.4 MB (11.6%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -5,11 +5,12 @@ class Solution {
         
         for(int i = 0; i < nums.length; i++) {
             ans[i] = nums[i];
+              ans[i + nums.length] = nums[i];
         }
         
-        for(int i = 0; i < nums.length; i++) {
-            ans[i + nums.length] = nums[i];
-        }
+        // for(int i = 0; i < nums.length; i++) {
+          
+        // }
         
         return ans;
     }
