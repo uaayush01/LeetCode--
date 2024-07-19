13f51e5103559629aab260a2390068648e1f7777
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-07-19 11:21:28 -0400

    Time: 0 ms (100%), Space: 41.5 MB (78.18%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,34 +1,14 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if(word.length()==1){
-            return true;
-        }
-        char firstChar = word.charAt(0);
-        boolean status = true;
-
-    if(Character.isLowerCase(firstChar)){
-        for(int i=1;i<word.length();i++){
-            if(Character.isUpperCase(word.charAt(i))){
-                status =false;
-                break;
-            }
-        }
-    }else if(Character.isUpperCase(firstChar) && Character.isLowerCase(word.charAt(1))){
-        for(int i=2;i<word.length();i++){
-            if(Character.isUpperCase(word.charAt(i))){
-                status =false;
-                break;
-            }
-        }
-    }else{
-        for(int i=2;i<word.length();i++){
-            if(Character.isLowerCase(word.charAt(i))){
-                status =false;
-                break;
-            }
-        }
+    int capital =0;
+    for(int i=0;i<word.length();i++){
+        if(word.charAt(i)<='Z') capital++;
     }
-     return status;
+    if(capital==word.length())  return true;
+    if(capital==1&& word.charAt(0)<='Z')  return true;
+    if(capital==0)  return true ;
+
+    return false;
 
     }
 }
