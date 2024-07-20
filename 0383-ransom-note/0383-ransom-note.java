49c2001176ea52197bc209d7fd70fa0bf6471920
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-07-20 11:09:27 -0400

    Time: 0 ms (100%), Space: 44.5 MB (82.38%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,26 +1,38 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap <Character,Integer> ans = new HashMap<>();
-       
-        for(int i=0;i<magazine.length();i++){
-            char c= magazine.charAt(i);
 
-            if(!ans.containsKey(c)){
-                ans.put(c,1);
-            }else{
-                ans.put(c,ans.get(c)+1);
-            }
-        }
-
-        for(int i=0;i<ransomNote.length();i++){
-            char c = ransomNote.charAt(i);
-
-            if(ans.containsKey(c) && ans.get(c)>0){
-                ans.put(c,ans.get(c)-1);
-            }else{
+        int [] map = new int [26];
+        for(char ch: ransomNote.toCharArray()){
+            int idx = magazine.indexOf(ch,map[ch%26]);
+            if(idx==-1){
                 return false;
             }
+            map[ch%26]= idx+1;
         }
         return true;
+
+
+        // HashMap <Character,Integer> ans = new HashMap<>();
+       
+        // for(int i=0;i<magazine.length();i++){
+        //     char c= magazine.charAt(i);
+
+        //     if(!ans.containsKey(c)){
+        //         ans.put(c,1);
+        //     }else{
+        //         ans.put(c,ans.get(c)+1);
+        //     }
+        // }
+
+        // for(int i=0;i<ransomNote.length();i++){
+        //     char c = ransomNote.charAt(i);
+
+        //     if(ans.containsKey(c) && ans.get(c)>0){
+        //         ans.put(c,ans.get(c)-1);
+        //     }else{
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 }
