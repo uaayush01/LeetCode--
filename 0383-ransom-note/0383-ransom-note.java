42bc9346d2e8dbe397b17a6dd614ba62ccc7c9c2
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-07-20 11:03:12 -0400

    Time: 15 ms (25.1%), Space: 45.1 MB (35.15%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap <Character,Integer> ans = new HashMap<>();
+       
+        for(int i=0;i<magazine.length();i++){
+            char c= magazine.charAt(i);
+
+            if(!ans.containsKey(c)){
+                ans.put(c,1);
+            }else{
+                ans.put(c,ans.get(c)+1);
+            }
+        }
+
+        for(int i=0;i<ransomNote.length();i++){
+            char c = ransomNote.charAt(i);
+
+            if(ans.containsKey(c) && ans.get(c)>0){
+                ans.put(c,ans.get(c)-1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
