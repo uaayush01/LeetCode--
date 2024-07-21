d49454857b83023ea0f67b00e4cf65cfb08ab7b0
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-07-21 11:03:10 -0400

    Time: 0 ms (100%), Space: 40.6 MB (82.61%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.java
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -6,5 +6,15 @@ class Solution {
         ans+=cnt[a]++;
       }
         return ans;
-    }
+    //  1ms approach
+    //  int ans = 0;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         for (int j = i + 1; j < nums.length; j++) {
+    //             if (nums[i] == nums[j]) {
+    //                 ans++;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+}
 }
