c406acfe5e409079d66c9f17662867fe0220596d
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-07-29 19:59:20 +0530

    Time: 0 ms (100%), Space: 42.6 MB (18.51%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -16,12 +16,13 @@ class Solution {
             if(list1.val<list2.val){
                 tail.next=list1;
                 list1=list1.next;
-                tail=tail.next;
+                
             }else{
                 tail.next=list2;
                 list2=list2.next;
-                tail=tail.next;
             }
+                tail=tail.next;
+
         }
          if (list1 == null) {
             tail.next = list2;
