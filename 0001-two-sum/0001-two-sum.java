d2d068f868916d870d97004fe48343c4a062a056
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-07-30 21:12:03 +0530

    Time: 45 ms (30.32%), Space: 45 MB (14.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,27 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=1;i<nums.length;i++)
-        {
-            for(int j=i;j<nums.length;j++)
-            {
-                if(nums[j]+nums[j-i]==target)
-                {
-                    return new int[]{j,j-i};
+        // bruteforce
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    int a[]={i,j};
+                    return a;
                 }
             }
         }
         return null;
+
+        // optimal
+        // for(int i=1;i<nums.length;i++)
+        // {
+        //     for(int j=i;j<nums.length;j++)
+        //     {
+        //         if(nums[j]+nums[j-i]==target)
+        //         {
+        //             return new int[]{j,j-i};
+        //         }
+        //     }
+        // }
+        // return null;
     }
 }
