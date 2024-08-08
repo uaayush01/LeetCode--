b4f5ad96307c105b6ef5b8547a978f888d301520
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-08-08 20:27:53 +0530

    Time: 0 ms (100%), Space: 41.5 MB (22.85%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -8,29 +8,38 @@ class MyQueue {
     }
     
     public void push(int x) {
-        first.push(x);
-    }
-    
-    public int pop() {
-        while(!first.isEmpty()){
-        second.push(first.pop());
-        }
-        int removed =second.pop();
-        while(!second.isEmpty()){
-            first.push(second.pop());
-        }
-        return removed;
-    }
-    
-    public int peek() {
+        // first.push(x);
         while(!first.isEmpty()){
             second.push(first.pop());
         }
-        int peeked=second.peek();
+        first.push(x);
         while(!second.isEmpty()){
             first.push(second.pop());
         }
-        return peeked;
+    }
+    
+    public int pop() {
+        // while(!first.isEmpty()){
+        // second.push(first.pop());
+        // }
+        // int removed =second.pop();
+        // while(!second.isEmpty()){
+        //     first.push(second.pop());
+        // }
+        // return removed;
+        return first.pop();
+    }
+    
+    public int peek() {
+        // while(!first.isEmpty()){
+        //     second.push(first.pop());
+        // }
+        // int peeked=second.peek();
+        // while(!second.isEmpty()){
+        //     first.push(second.pop());
+        // }
+        // return peeked;
+        return first.peek();
     }
     
     public boolean empty() {
