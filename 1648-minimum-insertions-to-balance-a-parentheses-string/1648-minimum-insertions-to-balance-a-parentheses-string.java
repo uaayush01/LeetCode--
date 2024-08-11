15bc83d373e1f8f46158026a58d14126a2a61908
AAYUSH UPADHYAY <144820397+uaayush01@users.noreply.github.com>
2024-08-11 20:41:09 +0530

    Time: 27 ms (42.65%), Space: 45.5 MB (35.31%) - LeetHub

--- 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.java
@@ -3,10 +3,10 @@ class Solution {
         Stack<Character> stack =new Stack<>();
         int count=0;
        for(int i=0;i<s.length();i++){
-        char c=s.charAt(i);
-        if(c=='('){
+       char c=s.charAt(i);
+          if(c=='('){
             stack.push(c);
-        }else{
+          }else{
             if(i+1<s.length()&&s.charAt(i+1)==')'){
                 i++;
             }else{
@@ -17,7 +17,7 @@ class Solution {
             }else{
                 count++;
             }
-        }
+          }
        }
        count+=2*stack.size();
        return count;
